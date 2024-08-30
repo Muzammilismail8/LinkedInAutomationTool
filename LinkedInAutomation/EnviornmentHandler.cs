4d7089a470c9fca71e6b42c083ee612487eb7a62
Muzammil Ismail <muzammilismail8@gmail.com>
2024-08-30 16:27:20 +0500

    finalize the project structure

---
+++ LinkedInAutomation/EnviornmentHandler.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Diagnostics;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
+
+namespace LinkedInAutomation;
+
+public static class EnviornmentHandler
+{
+    public static IWebDriver? ConfigureAndRunCrome()
+    {
+        try
+        {
+            foreach (var process in Process.GetProcessesByName("chrome"))
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            var options = new ChromeOptions();
+
+            string windowUserName = "Reacon";
+
+            options.AddArgument(@$"user-data-dir=C:\Users\{windowUserName}\AppData\Local\Google\Chrome\User Data");
+            options.AddArgument(@"profile-directory=Default");
+
+            IWebDriver? driver = new ChromeDriver(options);
+            return driver;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return null;
+    }
+
+}
--- LinkedInAutomation/Program.cs
+++ LinkedInAutomation/Program.cs
@@ -1,14 +1,8 @@
 ﻿using LinkedInAutomation;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Diagnostics;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
 
 Console.WriteLine("Starting LinkedIn Automation");
-
-IWebDriver? driver = ConfigureAndRunCrome();
-if(driver == null) { throw new NotFoundException(); }
+IWebDriver driver = EnviornmentHandler.ConfigureAndRunCrome()!;
 
 driver.Navigate().GoToUrl("https://www.linkedin.com/jobs/collections/easy-apply");
 Thread.Sleep(3000);
@@ -18,31 +12,5 @@ while (true)
     JobHandler.ApplyOnCurrentJob(driver);
 }
 
-static IWebDriver? ConfigureAndRunCrome()
-{
-    try
-    {
-        foreach (var process in Process.GetProcessesByName("chrome"))
-        {
-            process.Kill();
-            process.WaitForExit();
-        }
-
-        new DriverManager().SetUpDriver(new ChromeConfig());
-        var options = new ChromeOptions();
-
-        string windowUserName = "Reacon";
-
-        options.AddArgument(@$"user-data-dir=C:\Users\{windowUserName}\AppData\Local\Google\Chrome\User Data");
-        options.AddArgument(@"profile-directory=Default");
-
-        return new ChromeDriver(options);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-    return null;
-}
 
 //driver.Dispose();
