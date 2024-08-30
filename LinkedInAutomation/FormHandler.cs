22cc7c4c1dcc0d9d741eee53c2f5226f57cefe90
Muzammil Ismail <muzammilismail8@gmail.com>
2024-08-30 20:31:39 +0500

    .

--- LinkedInAutomation/FormHandler.cs
+++ LinkedInAutomation/FormHandler.cs
@@ -1,7 +1,6 @@
 ﻿using OpenQA.Selenium;
 
 namespace LinkedInAutomation;
-
 public static class FormHandler
 {
     public static void FillForm(IWebDriver driver)
@@ -11,20 +10,24 @@ public static class FormHandler
             var formData = new Dictionary<string, string>
         {
             { "email", "oliviagates16@gmail.com" },
+            { "CoverLetter", "oliviagates16@gmail.com" },
             { "phone", "3347479687" },
             { "countryCode", "+92" },
             { "Address", "Lahore, Pakistan" },
             { "Summary", "Full Stack .Net Devloper with 3 year of experience." }
         };
 
-            var phoneField = FindEmptyInputFieldByStrings(driver, ["phone", "Phone", "tel"]);
+            var phoneField = FindInputFieldByLabelContains(driver, ["phone", "Phone", "tel"]);
             if (phoneField != null) phoneField.SendKeys(formData["phone"]);
 
-            var addressField = FindEmptyInputFieldByStrings(driver, ["Address", "address", "ADDRESS"]);
-            if (phoneField != null) phoneField.SendKeys(formData["Address"]);
+            var addressField = FindInputFieldByLabelContains(driver, ["Address", "address", "ADDRESS"]);
+            if (addressField != null) addressField.SendKeys(formData["Address"]);
 
-            var summeryField = FindEmptyInputFieldByStrings(driver, ["Summary", "summary", "SUMMARY"]);
-            if (phoneField != null) phoneField.SendKeys(formData["phone"]);
+            var summeryField = FindInputFieldByLabelContains(driver, ["Summary", "summary", "SUMMARY"]);
+            if (summeryField != null) summeryField.SendKeys(formData["Summary"]);
+
+            var CoverLetterField = FindInputFieldByLabelContains(driver, ["Cover letter", "Cover letter", "COVER LETTER"]);
+            if (CoverLetterField != null) CoverLetterField.SendKeys(formData["CoverLetter"]);
         }
         catch (Exception ex)
         {
@@ -58,4 +61,45 @@ public static class FormHandler
 
         return null;
     }
+
+    static IWebElement? FindInputFieldByLabelContains(IWebDriver driver, string[] partialAttributeNames)
+    {
+        foreach (var partialAttributeName in partialAttributeNames)
+        {
+            try
+            {
+                IWebElement labelElement = driver.FindElement(By.XPath($"//label[contains(text(), '{partialAttributeName}')]"));
+
+                if (labelElement != null)
+                {
+                    try
+                    {
+                        string inputId = labelElement.GetAttribute("for");
+
+                        if (!string.IsNullOrEmpty(inputId))
+                        {
+                            IWebElement inputElement = driver.FindElement(By.Id(inputId));
+
+                            if (inputElement != null && string.IsNullOrEmpty(inputElement.GetAttribute("value")))
+                            {
+                                return inputElement;
+                            }
+                        }
+
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        Console.WriteLine($"Input attach with label {partialAttributeName} not found.");
+                    }
+                }
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine($"Label containing text '{partialAttributeName}' not found.");
+            }
+        }
+
+        return null;
+    }
+
 }
